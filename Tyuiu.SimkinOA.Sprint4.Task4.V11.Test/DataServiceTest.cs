f85757ab1c4e5c5d64be113c24c84a45984bbf7e
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-13 20:15:38 +0500

    Task4

---
+++ Tyuiu.SimkinOA.Sprint4.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.SimkinOA.Sprint4.Task4.V11.Lib;
+namespace Tyuiu.SimkinOA.Sprint4.Task4.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSum()
+        {
+            DataService ds = new DataService();
+
+            int[,] numbers = { { 5, 7, 7, 8, 5 }, { 6, 5, 6, 8, 6 }, { 7, 6, 8, 8, 5 }, { 7, 6, 7, 8, 6 }, { 7, 6, 7, 7, 5 } };
+            int res = 81;
+            int wait = ds.Calculate(numbers);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint4.Task4.V11/Program.cs
@@ -0,0 +1,71 @@
+﻿using Tyuiu.SimkinOA.Sprint4.Task4.V11.Lib;
+namespace Tyuiu.SimkinOA.Sprint4.Task4.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил Симкин О.А. | РППб-25-1";
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                 *");
+            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                               *");
+            Console.WriteLine("* Задание #4                                                                *");
+            Console.WriteLine("* Вариант #11                                                               *");
+            Console.WriteLine("* Выполнила: Симкин Олег Андреевич | РППб-25-1                              *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный          *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 5 до 8. Найдите сумму              *");
+            Console.WriteLine("* нечетных элементов массива.                                               *");
+            Console.WriteLine("*                                                                           *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("*****************************************************************************");
+
+            Console.Write("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] numbers = new int[rows, columns];
+
+            Console.WriteLine("*****************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите {i}, {j} элемент массива: ");
+                    numbers[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("\nМассив:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{numbers[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("*****************************************************************************");
+
+            int res = ds.Calculate(numbers);
+
+            Console.WriteLine("Сумма нечетных элементов = " + res);
+            Console.ReadKey();
+
+        }
+    }
+}
