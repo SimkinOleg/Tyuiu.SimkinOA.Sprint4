d6aa7b9e62782f5d34275b852443d0e42ce4dd29
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-13 19:16:34 +0500

    Task3

---
+++ Tyuiu.SimkinOA.Sprint4.Task3.V1.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SimkinOA.Sprint4.Task3.V1.Lib
+{
+    public class DataService : ISprint4Task3V1
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                sum += array[i, array.GetLength(1) - 1];
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint4.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SimkinOA.Sprint4.Task3.V1.Lib;
+namespace Tyuiu.SimkinOA.Sprint4.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCal()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] { { 3, 4, 3, 4, 1 },
+                                          { 8, 1, 6, 6, 9 },
+                                          { 9, 8, 5, 1, 3 },
+                                          { 1, 8, 7, 1, 2 },
+                                          { 9, 9, 7, 5, 6 } };
+            int res = ds.Calculate(mas2);
+            int wait = 21;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
