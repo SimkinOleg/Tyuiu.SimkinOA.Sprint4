865f863b0ea7876fbcea2e2026b3378782e77dee
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-13 12:46:17 +0500

    Task1

--- Tyuiu.SimkinOA.Sprint4.Task1.V7.Lib/DataService.cs
+++ Tyuiu.SimkinOA.Sprint4.Task1.V7.Lib/DataService.cs
@@ -4,21 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using tyuiu.cources.programming.interfaces.Sprint4;
-namespace Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib
+namespace Tyuiu.SimkinOA.Sprint4.Task1.V7.Lib
 {
-    public class DataService : ISprint4Task0V13
+    public class DataService : ISprint4Task1V7
     {
-        public int GetSumEvenArrEl(int[] array)
+        public int Calculate(int[] array)
         {
-            int sum = 0;
-            for (int i = 0; i < array.Length; i++)
+            int s = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 == 0)
                 {
-                    sum += array[i];
+                    s += array[i];
                 }
             }
-            return sum;
+            return s;
         }
     }
 }
