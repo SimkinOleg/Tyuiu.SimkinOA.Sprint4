80242ef3c62844ca3b49bc38c5dbe87112feec49
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-14 00:11:16 +0500

    Task0

--- Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib/DataService.cs
+++ Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib/DataService.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using tyuiu.cources.programming.interfaces.Sprint4;
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 namespace Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib
-{
+{{
     public class DataService : ISprint4Task0V13
     {
         public int GetSumEvenArrEl(int[] array)
         {
             int sum = 0;
-            for (int i = 0; i < array.Length; i++)
+
+            for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 == 0)
                 {
---
+++ Tyuiu.SimkinOA.Sprint4.Task0.V13.Test/Test1.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib;
+namespace Tyuiu.SimkinOA.Sprint4.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumEvenArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+            int res = ds.GetSumEvenArrEl(numsArray);
+            int numsWaitArray = 26;
+            Assert.AreEqual(numsWaitArray, res);
+        }
+    }
+}
