285a5a83fc62f66ef577c44f6b3dcf0999d3f98f
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-13 16:37:52 +0500

    Task2

---
+++ Tyuiu.SimkinOA.Sprint4.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SimkinOA.Sprint4.Task2.V14.Lib;
+namespace Tyuiu.SimkinOA.Sprint4.Task2.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidMulti()
+        {
+            DataService ds = new DataService();
+
+            int[] numbers = { 4, 8, 1, 7, 7, 3, 1, 5, 8, 2, 2, 1 };
+
+            int res = ds.Calculate(numbers);
+            int wait = 735;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint4.Task2.V14/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.SimkinOA.Sprint4.Task2.V14.Lib;
+namespace Tyuiu.SimkinOA.Sprint4.Task2.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил Симкин О.А. | РППб-25-1";
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                 *");
+            Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                      *");
+            Console.WriteLine("* Задание #2                                                                *");
+            Console.WriteLine("* Вариант #14                                                               *");
+            Console.WriteLine("* Выполнил: Симкин Олег Андреевич | РППб-25-1                               *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов, заполненный          *");
+            Console.WriteLine("* случайными значениями в диапазоне от 1 до 8. Подсчитать произведение      *");
+            Console.WriteLine("* нечетных элементов массива.                                               *");
+            Console.WriteLine("*                                                                           *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("*****************************************************************************");
+
+            Console.Write("Введите количество элементов массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] numbers = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                numbers[i] = rnd.Next(1, 8);
+            }
+
+            Console.WriteLine("Массив: ");
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(numbers[i] + "\t");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("*****************************************************************************");
+
+            int res = ds.Calculate(numbers);
+            Console.WriteLine("Произведение нечетных элементов: " + res);
+            Console.ReadKey();
+        }
+    }
+}
