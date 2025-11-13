93139f42d7bce6919b20e451c8c47ced4214ce5c
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-13 23:04:52 +0500

    Task6

---
+++ Tyuiu.SimkinOA.Sprint4.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SimkinOA.Sprint4.Task6.V11.Lib
+{
+    public class DataService : ISprint4Task6V11
+    {
+        public int Calculate(string[] array)
+        {
+            int count = 0;
+            
+            for(int i = 0; i < array.Length; i++) 
+            {
+                if (array[i].Length ==5)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint4.Task6.V11/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.SimkinOA.Sprint4.Task6.V11.Lib;
+namespace Tyuiu.SimkinOA.Sprint4.Task6.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Симкин О.А. | РППб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Симкин Олег Андреевич | РППб-25-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*Дан строковый массив данных [\"Кошка\", \"Собака\", \"Слон\", \"          ");
+            Console.WriteLine("Жираф\", \"Бегемот\", \"Игуана\", \"Ягуар\"] используя                     ");
+            Console.WriteLine("класс Array подсчитайте количество элементов, длина которых равна 5.*      ");
+                                   
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            string[] myarray = { "Кошка", "Собака", "Слон", "Жираф", "Бегемот", "Игуана", "Ягуар" };
+            var result = ds.Calculate(myarray);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
