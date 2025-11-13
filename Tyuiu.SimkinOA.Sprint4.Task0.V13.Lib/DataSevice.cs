9c42b3f82e7b3ebcec1992106330a2fb2511af0a
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-13 12:16:53 +0500

    Task0

--- Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib/DataSevice.cs
+++ Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib/DataSevice.cs
@@ -6,7 +6,8 @@ namespace Tyuiu.SimkinOA.Sprint4.Task0.V13.Lib
         public int GetSumEvenArrEl(int[] array)
         {
             int sum = 0;
-            for (int i = 0; i < array.Length; i++)
+
+            for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 == 0)
                 {
